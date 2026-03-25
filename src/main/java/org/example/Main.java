a81c7c1ef705ec7f75b532d34b95f07ce3590402
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-03-25 13:20:38 +0100

    subir2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,15 +13,47 @@ public class Main {
                 DBConfig.getUser(),
                 DBConfig.getPassword()
         );  Statement statement = connection.createStatement()) {
-            System.out.println("Conectado con Oracle.");
 
             //Línea para crear la tabla
-            String sql = "CREATE TABLE empleado ("+
-                    "id NUMBER PRIMARY KEY, "+
-                    "nombre VARCHAR(100), "+
-                    "salario NUMBER(10,2))";
-            statement.executeUpdate(sql); //Ejecutar la línea
-            System.out.println("Tabla creada.");
+            String tablaDep =
+                    "CREATE TABLE DEPARTAMENTO \n" +
+                    "    ( \n" +
+                    "     ID_dep     NUMBER (3)  NOT NULL , \n" +
+                    "     nombre_dep VARCHAR2 (100)  NOT NULL \n" +
+                    "    ) ";
+            statement.executeUpdate(tablaDep);//Ejecutar la línea
+
+            //Alterar tabla departamento --> Clave primaria
+            String alterDep = "ALTER TABLE DEPARTAMENTO \n" +
+                    "    ADD CONSTRAINT DEPARTAMENTO_PK PRIMARY KEY ( ID_dep )";
+            statement.execute(alterDep);
+
+            System.out.println("Tabla Departamento creada.");
+
+            String tableEmp = "CREATE TABLE EMPLEADO2 \n" +
+                    "    ( \n" +
+                    "     ID_emp              NUMBER  NOT NULL , \n" +
+                    "     nombre_emp          VARCHAR2 (100)  NOT NULL , \n" +
+                    "     ID_dep NUMBER (3)   NOT NULL \n" +
+                    "    )";
+            statement.execute(tableEmp);
+
+            String alterEmp1 = "ALTER TABLE EMPLEADO2 \n" +
+                    "    ADD CONSTRAINT EMPLEADO2_PK PRIMARY KEY ( ID_emp )";
+            statement.execute(alterEmp1);
+
+            //Alterar tabla empelado para establecer clave foranea con dep
+            String alterEmp2 = "ALTER TABLE EMPLEADO2 \n" +
+                    "    ADD CONSTRAINT EMPLEADO2_DEPARTAMENTO_FK FOREIGN KEY \n" +
+                    "    ( \n" +
+                    "     ID_dep\n" +
+                    "    ) \n" +
+                    "    REFERENCES DEPARTAMENTO \n" +
+                    "    ( \n" +
+                    "     ID_dep\n" +
+                    "    ) \n";
+            statement.execute(alterEmp2);
+            System.out.println("Tabla Empleado creada.");
 
         } catch (SQLException e){
             System.out.println("ERROR --> "+e.getMessage());
